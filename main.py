4ae203e3034283e37fed4a8d4221a52a1b95577d
green-waaagh <green.waaagh@gmail.com>
2025-10-20 23:42:23 +0300

    add arguments and exeptions

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from stats import *
+import sys
+
+try:
+    book_addres = sys.argv[1]
+except:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def main():
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_addres}...")
     print("----------- Word Count ----------")
-    num_words=get_num_words("books/frankenstein.txt")
+    num_words=get_num_words(book_addres)
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    dict=get_num_characters("books/frankenstein.txt")
-    letters_list=sort_list("books/frankenstein.txt")
+    dict=get_num_characters(book_addres)
+    letters_list=sort_list(book_addres)
     for i in letters_list:
         print(f"{i['char']}: {i['num']}")
     print("============= END ===============")
